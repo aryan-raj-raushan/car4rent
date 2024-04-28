b1f07bff397a25c2094d605c14553acaa06ffe96
aryan-raj-raushan <aryanraj.raushan@gmail.com>
2024-04-28 14:28:39 +0530

    some fixes

---
+++ src/components/Svgs/Warning.jsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+const WarningSvg = (props) => {
+  return (
+    <svg
+      width="77"
+      height="67"
+      viewBox="0 0 77 67"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <title>Warning</title>
+      <desc>Created with Sketch</desc>
+      <defs>
+        <polygon
+          id="path-1"
+          points="34.8913043 0 0 59.673913 68.8043478 59.673913"
+        ></polygon>
+      </defs>
+      <g
+        id="Errors/empty-states"
+        stroke="none"
+        strokeWidth="1"
+        fill="none"
+        fillRule="evenodd"
+      >
+        <g
+          id="No-flights-found-due-to-too-many-filters"
+          transform="translate(-802.000000, -384.000000)"
+        >
+          <g id="Group-2" transform="translate(806.000000, 388.000000)">
+            <g id="Group-8">
+              <g id="Path-2" strokeLinejoin="round">
+                <use
+                  fill="#E5E5E5"
+                  fillRule="evenodd"
+                  xlinkHref="#path-1"
+                ></use>
+                <path
+                  stroke="#E5E5E5"
+                  strokeWidth="3.26086957"
+                  d="M34.9113881,-3.26452744 L71.6062681,61.3043478 L-2.84199794,61.3043478 L34.9113881,-3.26452744 Z"
+                ></path>
+                <path
+                  stroke="#FFFFFF"
+                  strokeWidth="3.26086957"
+                  d="M34.8712205,3.26452744 L2.84199794,58.0434783 L66.0024276,58.0434783 L34.8712205,3.26452744 Z"
+                  strokeLinejoin="square"
+                ></path>
+              </g>
+              <path
+                d="M35.8185292,45.008322 L36.7547554,32.8564878 L36.7547554,23.6279721 L32.8761039,23.6279721 L32.8761039,32.8564878 L33.8696501,45.008322 L35.8185292,45.008322 Z M36.6783288,51.6956522 L36.6783288,47.7214674 L32.8761039,47.7214674 L32.8761039,51.6956522 L36.6783288,51.6956522 Z"
+                id="!"
+                fill="#CCCCCC"
+                fillRule="nonzero"
+              ></path>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default WarningSvg;
